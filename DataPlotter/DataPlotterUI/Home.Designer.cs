e7c0aad321f120545e0d61b271aa7e20cb059eb9
Gautzilla <gauthier.berthomieu@gmail.com>
2022-11-30 11:36:58 +0100

    Add chart size control

--- DataPlotter/DataPlotterUI/Home.Designer.cs
+++ DataPlotter/DataPlotterUI/Home.Designer.cs
@@ -36,12 +36,16 @@
             this.ListBox_xVar = new System.Windows.Forms.ListBox();
             this.label_yVar = new System.Windows.Forms.Label();
             this.ListBox_yVar = new System.Windows.Forms.ListBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.Label_yVar2 = new System.Windows.Forms.Label();
             this.ListBox_yVar2 = new System.Windows.Forms.ListBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.Label_yVar2Levels = new System.Windows.Forms.Label();
             this.ListBox_yVar2Levels = new System.Windows.Forms.ListBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.TextBox_chartWidth = new System.Windows.Forms.TextBox();
+            this.TextBox_chartHeight = new System.Windows.Forms.TextBox();
+            this.Label_chartWidth = new System.Windows.Forms.Label();
+            this.Label_chartHeight = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label_xVar
@@ -132,15 +136,15 @@
             this.ListBox_yVar.TabIndex = 4;
             this.ListBox_yVar.SelectedIndexChanged += new System.EventHandler(this.ListBox_yVar_SelectedIndexChanged);
             // 
-            // label2
+            // Label_yVar2
             // 
-            this.label2.AutoSize = true;
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(215)))), ((int)(((byte)(201)))));
-            this.label2.Location = new System.Drawing.Point(480, 206);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(164, 19);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "2nd y-axis variable";
+            this.Label_yVar2.AutoSize = true;
+            this.Label_yVar2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(215)))), ((int)(((byte)(201)))));
+            this.Label_yVar2.Location = new System.Drawing.Point(480, 206);
+            this.Label_yVar2.Name = "Label_yVar2";
+            this.Label_yVar2.Size = new System.Drawing.Size(164, 19);
+            this.Label_yVar2.TabIndex = 1;
+            this.Label_yVar2.Text = "2nd y-axis variable";
             // 
             // ListBox_yVar2
             // 
@@ -154,15 +158,15 @@
             this.ListBox_yVar2.TabIndex = 4;
             this.ListBox_yVar2.SelectedIndexChanged += new System.EventHandler(this.ListBox_yVar2_SelectedIndexChanged);
             // 
-            // label1
+            // Label_yVar2Levels
             // 
-            this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(215)))), ((int)(((byte)(201)))));
-            this.label1.Location = new System.Drawing.Point(480, 339);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(208, 19);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "2nd y-axis variable level";
+            this.Label_yVar2Levels.AutoSize = true;
+            this.Label_yVar2Levels.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(215)))), ((int)(((byte)(201)))));
+            this.Label_yVar2Levels.Location = new System.Drawing.Point(480, 339);
+            this.Label_yVar2Levels.Name = "Label_yVar2Levels";
+            this.Label_yVar2Levels.Size = new System.Drawing.Size(208, 19);
+            this.Label_yVar2Levels.TabIndex = 1;
+            this.Label_yVar2Levels.Text = "2nd y-axis variable level";
             // 
             // ListBox_yVar2Levels
             // 
@@ -184,23 +188,65 @@
             // 
             this.openFileDialog2.FileName = "openFileDialog2";
             // 
+            // TextBox_chartWidth
+            // 
+            this.TextBox_chartWidth.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(78)))), ((int)(((byte)(79)))));
+            this.TextBox_chartWidth.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(123)))), ((int)(((byte)(92)))));
+            this.TextBox_chartWidth.Location = new System.Drawing.Point(12, 451);
+            this.TextBox_chartWidth.Name = "TextBox_chartWidth";
+            this.TextBox_chartWidth.Size = new System.Drawing.Size(170, 27);
+            this.TextBox_chartWidth.TabIndex = 5;
+            // 
+            // TextBox_chartHeight
+            // 
+            this.TextBox_chartHeight.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(78)))), ((int)(((byte)(79)))));
+            this.TextBox_chartHeight.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(123)))), ((int)(((byte)(92)))));
+            this.TextBox_chartHeight.Location = new System.Drawing.Point(188, 451);
+            this.TextBox_chartHeight.Name = "TextBox_chartHeight";
+            this.TextBox_chartHeight.Size = new System.Drawing.Size(170, 27);
+            this.TextBox_chartHeight.TabIndex = 5;
+            // 
+            // Label_chartWidth
+            // 
+            this.Label_chartWidth.AutoSize = true;
+            this.Label_chartWidth.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(215)))), ((int)(((byte)(201)))));
+            this.Label_chartWidth.Location = new System.Drawing.Point(12, 481);
+            this.Label_chartWidth.Name = "Label_chartWidth";
+            this.Label_chartWidth.Size = new System.Drawing.Size(105, 19);
+            this.Label_chartWidth.TabIndex = 1;
+            this.Label_chartWidth.Text = "Chart width";
+            // 
+            // Label_chartHeight
+            // 
+            this.Label_chartHeight.AutoSize = true;
+            this.Label_chartHeight.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(215)))), ((int)(((byte)(201)))));
+            this.Label_chartHeight.Location = new System.Drawing.Point(188, 481);
+            this.Label_chartHeight.Name = "Label_chartHeight";
+            this.Label_chartHeight.Size = new System.Drawing.Size(111, 19);
+            this.Label_chartHeight.TabIndex = 1;
+            this.Label_chartHeight.Text = "Chart height";
+            // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(54)))), ((int)(((byte)(57)))));
             this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.TextBox_chartHeight);
+            this.Controls.Add(this.TextBox_chartWidth);
             this.Controls.Add(this.ListBox_yVar2Levels);
             this.Controls.Add(this.ListBox_yVar2);
             this.Controls.Add(this.ListBox_yVar);
             this.Controls.Add(this.ListBox_xVar);
             this.Controls.Add(this.label_infoPath);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Label_yVar2Levels);
             this.Controls.Add(this.label_dataPath);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Label_yVar2);
             this.Controls.Add(this.btn_InfoPath);
             this.Controls.Add(this.label_yVar);
             this.Controls.Add(this.btn_dataFilePath);
+            this.Controls.Add(this.Label_chartHeight);
+            this.Controls.Add(this.Label_chartWidth);
             this.Controls.Add(this.label_xVar);
             this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(5);
@@ -220,12 +266,16 @@
         private System.Windows.Forms.ListBox ListBox_xVar;
         private System.Windows.Forms.Label label_yVar;
         private System.Windows.Forms.ListBox ListBox_yVar;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Label_yVar2;
         private System.Windows.Forms.ListBox ListBox_yVar2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Label_yVar2Levels;
         private System.Windows.Forms.ListBox ListBox_yVar2Levels;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.TextBox TextBox_chartWidth;
+        private System.Windows.Forms.TextBox TextBox_chartHeight;
+        private System.Windows.Forms.Label Label_chartWidth;
+        private System.Windows.Forms.Label Label_chartHeight;
     }
 }
 
