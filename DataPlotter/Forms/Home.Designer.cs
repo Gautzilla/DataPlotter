a301ed2f6c9a38f2dc448f5d8725ddbee5def329
Gautzilla <gauthier.berthomieu@gmail.com>
2023-06-05 16:23:07 +0200

    Add main menu button logic

--- DataPlotter/Forms/Home.Designer.cs
+++ DataPlotter/Forms/Home.Designer.cs
@@ -29,16 +29,18 @@
         private void InitializeComponent()
         {
             this.panelMenu = new System.Windows.Forms.Panel();
-            this.panelLogo = new System.Windows.Forms.Panel();
-            this.buttonHome = new System.Windows.Forms.Button();
-            this.buttonData = new System.Windows.Forms.Button();
-            this.buttonVariables = new System.Windows.Forms.Button();
-            this.buttonChart = new System.Windows.Forms.Button();
             this.buttonPlot = new System.Windows.Forms.Button();
+            this.buttonChart = new System.Windows.Forms.Button();
+            this.buttonVariables = new System.Windows.Forms.Button();
+            this.buttonData = new System.Windows.Forms.Button();
+            this.buttonSet = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
             this.labelLogo = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.labelActiveForm = new System.Windows.Forms.Label();
             this.panelMenu.SuspendLayout();
             this.panelLogo.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelMenu
@@ -48,13 +50,95 @@
             this.panelMenu.Controls.Add(this.buttonChart);
             this.panelMenu.Controls.Add(this.buttonVariables);
             this.panelMenu.Controls.Add(this.buttonData);
-            this.panelMenu.Controls.Add(this.buttonHome);
+            this.panelMenu.Controls.Add(this.buttonSet);
             this.panelMenu.Controls.Add(this.panelLogo);
             this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelMenu.Location = new System.Drawing.Point(0, 0);
             this.panelMenu.Name = "panelMenu";
             this.panelMenu.Size = new System.Drawing.Size(170, 450);
             this.panelMenu.TabIndex = 0;
+            // 
+            // buttonPlot
+            // 
+            this.buttonPlot.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonPlot.FlatAppearance.BorderSize = 0;
+            this.buttonPlot.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPlot.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Bold);
+            this.buttonPlot.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
+            this.buttonPlot.Location = new System.Drawing.Point(0, 400);
+            this.buttonPlot.Name = "buttonPlot";
+            this.buttonPlot.Size = new System.Drawing.Size(170, 50);
+            this.buttonPlot.TabIndex = 5;
+            this.buttonPlot.Text = "Plot";
+            this.buttonPlot.UseVisualStyleBackColor = true;
+            // 
+            // buttonChart
+            // 
+            this.buttonChart.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonChart.FlatAppearance.BorderSize = 0;
+            this.buttonChart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonChart.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Bold);
+            this.buttonChart.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
+            this.buttonChart.Location = new System.Drawing.Point(0, 230);
+            this.buttonChart.Name = "buttonChart";
+            this.buttonChart.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.buttonChart.Size = new System.Drawing.Size(170, 50);
+            this.buttonChart.TabIndex = 4;
+            this.buttonChart.Text = "Chart";
+            this.buttonChart.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonChart.UseVisualStyleBackColor = true;
+            this.buttonChart.Click += new System.EventHandler(this.buttonChart_Click);
+            // 
+            // buttonVariables
+            // 
+            this.buttonVariables.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonVariables.FlatAppearance.BorderSize = 0;
+            this.buttonVariables.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonVariables.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Bold);
+            this.buttonVariables.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
+            this.buttonVariables.Location = new System.Drawing.Point(0, 180);
+            this.buttonVariables.Name = "buttonVariables";
+            this.buttonVariables.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.buttonVariables.Size = new System.Drawing.Size(170, 50);
+            this.buttonVariables.TabIndex = 3;
+            this.buttonVariables.Text = "Variables";
+            this.buttonVariables.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonVariables.UseVisualStyleBackColor = true;
+            this.buttonVariables.Click += new System.EventHandler(this.buttonVariables_Click);
+            // 
+            // buttonData
+            // 
+            this.buttonData.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonData.FlatAppearance.BorderSize = 0;
+            this.buttonData.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonData.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Bold);
+            this.buttonData.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
+            this.buttonData.Location = new System.Drawing.Point(0, 130);
+            this.buttonData.Name = "buttonData";
+            this.buttonData.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.buttonData.Size = new System.Drawing.Size(170, 50);
+            this.buttonData.TabIndex = 2;
+            this.buttonData.Text = "Data";
+            this.buttonData.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonData.UseVisualStyleBackColor = true;
+            this.buttonData.Click += new System.EventHandler(this.buttonData_Click);
+            // 
+            // buttonSet
+            // 
+            this.buttonSet.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonSet.FlatAppearance.BorderSize = 0;
+            this.buttonSet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSet.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Bold);
+            this.buttonSet.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
+            this.buttonSet.Location = new System.Drawing.Point(0, 80);
+            this.buttonSet.Name = "buttonSet";
+            this.buttonSet.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.buttonSet.Size = new System.Drawing.Size(170, 50);
+            this.buttonSet.TabIndex = 1;
+            this.buttonSet.Text = "Set";
+            this.buttonSet.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonSet.UseVisualStyleBackColor = true;
+            this.buttonSet.Click += new System.EventHandler(this.buttonSet_Click);
             // 
             // panelLogo
             // 
@@ -66,84 +150,6 @@
             this.panelLogo.Name = "panelLogo";
             this.panelLogo.Size = new System.Drawing.Size(170, 80);
             this.panelLogo.TabIndex = 0;
-            // 
-            // buttonHome
-            // 
-            this.buttonHome.Dock = System.Windows.Forms.DockStyle.Top;
-            this.buttonHome.FlatAppearance.BorderSize = 0;
-            this.buttonHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonHome.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonHome.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
-            this.buttonHome.Location = new System.Drawing.Point(0, 80);
-            this.buttonHome.Name = "buttonHome";
-            this.buttonHome.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.buttonHome.Size = new System.Drawing.Size(170, 50);
-            this.buttonHome.TabIndex = 1;
-            this.buttonHome.Text = "Home";
-            this.buttonHome.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonHome.UseVisualStyleBackColor = true;
-            // 
-            // buttonData
-            // 
-            this.buttonData.Dock = System.Windows.Forms.DockStyle.Top;
-            this.buttonData.FlatAppearance.BorderSize = 0;
-            this.buttonData.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonData.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonData.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
-            this.buttonData.Location = new System.Drawing.Point(0, 130);
-            this.buttonData.Name = "buttonData";
-            this.buttonData.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.buttonData.Size = new System.Drawing.Size(170, 50);
-            this.buttonData.TabIndex = 2;
-            this.buttonData.Text = "Data";
-            this.buttonData.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonData.UseVisualStyleBackColor = true;
-            // 
-            // buttonVariables
-            // 
-            this.buttonVariables.Dock = System.Windows.Forms.DockStyle.Top;
-            this.buttonVariables.FlatAppearance.BorderSize = 0;
-            this.buttonVariables.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonVariables.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonVariables.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
-            this.buttonVariables.Location = new System.Drawing.Point(0, 180);
-            this.buttonVariables.Name = "buttonVariables";
-            this.buttonVariables.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.buttonVariables.Size = new System.Drawing.Size(170, 50);
-            this.buttonVariables.TabIndex = 3;
-            this.buttonVariables.Text = "Variables";
-            this.buttonVariables.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonVariables.UseVisualStyleBackColor = true;
-            // 
-            // buttonChart
-            // 
-            this.buttonChart.Dock = System.Windows.Forms.DockStyle.Top;
-            this.buttonChart.FlatAppearance.BorderSize = 0;
-            this.buttonChart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonChart.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonChart.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
-            this.buttonChart.Location = new System.Drawing.Point(0, 230);
-            this.buttonChart.Name = "buttonChart";
-            this.buttonChart.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.buttonChart.Size = new System.Drawing.Size(170, 50);
-            this.buttonChart.TabIndex = 4;
-            this.buttonChart.Text = "Chart";
-            this.buttonChart.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonChart.UseVisualStyleBackColor = true;
-            // 
-            // buttonPlot
-            // 
-            this.buttonPlot.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.buttonPlot.FlatAppearance.BorderSize = 0;
-            this.buttonPlot.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonPlot.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonPlot.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
-            this.buttonPlot.Location = new System.Drawing.Point(0, 400);
-            this.buttonPlot.Name = "buttonPlot";
-            this.buttonPlot.Size = new System.Drawing.Size(170, 50);
-            this.buttonPlot.TabIndex = 5;
-            this.buttonPlot.Text = "Plot";
-            this.buttonPlot.UseVisualStyleBackColor = true;
             // 
             // labelLogo
             // 
@@ -159,11 +165,24 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(66)))), ((int)(((byte)(89)))));
+            this.panel1.Controls.Add(this.labelActiveForm);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(170, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(630, 80);
             this.panel1.TabIndex = 1;
+            // 
+            // labelActiveForm
+            // 
+            this.labelActiveForm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelActiveForm.Font = new System.Drawing.Font("Consolas", 16F, System.Drawing.FontStyle.Bold);
+            this.labelActiveForm.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
+            this.labelActiveForm.Location = new System.Drawing.Point(0, 0);
+            this.labelActiveForm.Name = "labelActiveForm";
+            this.labelActiveForm.Size = new System.Drawing.Size(630, 80);
+            this.labelActiveForm.TabIndex = 0;
+            this.labelActiveForm.Text = "SET";
+            this.labelActiveForm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Home
             // 
@@ -176,6 +195,7 @@
             this.Text = "Home";
             this.panelMenu.ResumeLayout(false);
             this.panelLogo.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -187,9 +207,10 @@
         private System.Windows.Forms.Button buttonChart;
         private System.Windows.Forms.Button buttonVariables;
         private System.Windows.Forms.Button buttonData;
-        private System.Windows.Forms.Button buttonHome;
+        private System.Windows.Forms.Button buttonSet;
         private System.Windows.Forms.Panel panelLogo;
         private System.Windows.Forms.Label labelLogo;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelActiveForm;
     }
 }
--- DataPlotter/Forms/Home.cs
+++ DataPlotter/Forms/Home.cs
@@ -12,9 +12,52 @@ namespace DataPlotter.Forms
 {
     public partial class Home : Form
     {
+        private Button currentButton;
+        private Color activeButtonColor = Color.FromArgb(246, 186, 111);
+        private Color unactiveButtonColor = Color.FromArgb(245, 239, 231);
+        
         public Home()
         {
             InitializeComponent();
+        }
+
+        private void ChangeActiveForm(Form newForm, object buttonSender)
+        {
+            SetCurrentButton(buttonSender);
+
+            labelActiveForm.Text = newForm.Text.ToUpper();
+            newForm.Dock = DockStyle.Fill;
+        }
+
+        private void SetCurrentButton(object buttonSender)
+        {
+            if (currentButton != null)
+            {
+                currentButton.ForeColor = unactiveButtonColor;
+            }
+
+            currentButton = (Button)buttonSender;
+            currentButton.ForeColor = activeButtonColor;
+        }
+
+        private void buttonSet_Click(object sender, EventArgs e)
+        {
+            ChangeActiveForm(new Set(), sender);
+        }
+
+        private void buttonData_Click(object sender, EventArgs e)
+        {
+            ChangeActiveForm(new Data(), sender);
+        }
+
+        private void buttonVariables_Click(object sender, EventArgs e)
+        {
+            ChangeActiveForm(new Variables(), sender);
+        }
+
+        private void buttonChart_Click(object sender, EventArgs e)
+        {
+            ChangeActiveForm(new Chart(), sender);
         }
     }
 }
