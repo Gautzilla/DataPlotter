d626ea6ee40afbed153e69be90b2ec55f2d929a8
Gautzilla <gauthier.berthomieu@gmail.com>
2023-06-07 15:15:17 +0200

    Add set form interface

--- DataPlotter/Forms/Set.Designer.cs
+++ DataPlotter/Forms/Set.Designer.cs
@@ -28,20 +28,254 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelVariablesInfoFile = new System.Windows.Forms.Label();
+            this.buttonBrowseInfofile = new System.Windows.Forms.Button();
+            this.checkBoxInfoFileLoaded = new System.Windows.Forms.CheckBox();
+            this.labelDataFile = new System.Windows.Forms.Label();
+            this.buttonBrowseDatafile = new System.Windows.Forms.Button();
+            this.checkBoxDataFileLoaded = new System.Windows.Forms.CheckBox();
+            this.buttonRecallPreset = new System.Windows.Forms.Button();
+            this.panelFilesGlobal = new System.Windows.Forms.Panel();
+            this.panelFiles = new System.Windows.Forms.Panel();
+            this.panelInfofile = new System.Windows.Forms.Panel();
+            this.panelDataFile = new System.Windows.Forms.Panel();
+            this.panelInfoFileButtons = new System.Windows.Forms.Panel();
+            this.buttonEditInfoFile = new System.Windows.Forms.Button();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.buttonGatherData = new System.Windows.Forms.Button();
+            this.panelFilesGlobal.SuspendLayout();
+            this.panelFiles.SuspendLayout();
+            this.panelInfofile.SuspendLayout();
+            this.panelDataFile.SuspendLayout();
+            this.panelInfoFileButtons.SuspendLayout();
+            this.panel8.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // labelVariablesInfoFile
+            // 
+            this.labelVariablesInfoFile.Dock = System.Windows.Forms.DockStyle.Left;
+            this.labelVariablesInfoFile.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+            this.labelVariablesInfoFile.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
+            this.labelVariablesInfoFile.Location = new System.Drawing.Point(0, 0);
+            this.labelVariablesInfoFile.Name = "labelVariablesInfoFile";
+            this.labelVariablesInfoFile.Padding = new System.Windows.Forms.Padding(10);
+            this.labelVariablesInfoFile.Size = new System.Drawing.Size(118, 90);
+            this.labelVariablesInfoFile.TabIndex = 0;
+            this.labelVariablesInfoFile.Text = "Variables info file";
+            this.labelVariablesInfoFile.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // buttonBrowseInfofile
+            // 
+            this.buttonBrowseInfofile.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonBrowseInfofile.FlatAppearance.BorderSize = 0;
+            this.buttonBrowseInfofile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonBrowseInfofile.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+            this.buttonBrowseInfofile.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
+            this.buttonBrowseInfofile.Location = new System.Drawing.Point(0, 0);
+            this.buttonBrowseInfofile.Name = "buttonBrowseInfofile";
+            this.buttonBrowseInfofile.Size = new System.Drawing.Size(107, 47);
+            this.buttonBrowseInfofile.TabIndex = 1;
+            this.buttonBrowseInfofile.Text = "Browse";
+            this.buttonBrowseInfofile.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxInfoFileLoaded
+            // 
+            this.checkBoxInfoFileLoaded.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkBoxInfoFileLoaded.Enabled = false;
+            this.checkBoxInfoFileLoaded.FlatAppearance.BorderSize = 0;
+            this.checkBoxInfoFileLoaded.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBoxInfoFileLoaded.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
+            this.checkBoxInfoFileLoaded.Location = new System.Drawing.Point(225, 0);
+            this.checkBoxInfoFileLoaded.Name = "checkBoxInfoFileLoaded";
+            this.checkBoxInfoFileLoaded.Padding = new System.Windows.Forms.Padding(10);
+            this.checkBoxInfoFileLoaded.Size = new System.Drawing.Size(53, 90);
+            this.checkBoxInfoFileLoaded.TabIndex = 2;
+            this.checkBoxInfoFileLoaded.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBoxInfoFileLoaded.UseVisualStyleBackColor = true;
+            // 
+            // labelDataFile
+            // 
+            this.labelDataFile.Dock = System.Windows.Forms.DockStyle.Left;
+            this.labelDataFile.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+            this.labelDataFile.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
+            this.labelDataFile.Location = new System.Drawing.Point(0, 0);
+            this.labelDataFile.Name = "labelDataFile";
+            this.labelDataFile.Padding = new System.Windows.Forms.Padding(10);
+            this.labelDataFile.Size = new System.Drawing.Size(118, 77);
+            this.labelDataFile.TabIndex = 0;
+            this.labelDataFile.Text = "Data File";
+            this.labelDataFile.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // buttonBrowseDatafile
+            // 
+            this.buttonBrowseDatafile.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonBrowseDatafile.FlatAppearance.BorderSize = 0;
+            this.buttonBrowseDatafile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonBrowseDatafile.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+            this.buttonBrowseDatafile.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
+            this.buttonBrowseDatafile.Location = new System.Drawing.Point(0, 0);
+            this.buttonBrowseDatafile.Name = "buttonBrowseDatafile";
+            this.buttonBrowseDatafile.Size = new System.Drawing.Size(107, 77);
+            this.buttonBrowseDatafile.TabIndex = 1;
+            this.buttonBrowseDatafile.Text = "Browse";
+            this.buttonBrowseDatafile.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxDataFileLoaded
+            // 
+            this.checkBoxDataFileLoaded.AutoSize = true;
+            this.checkBoxDataFileLoaded.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkBoxDataFileLoaded.Enabled = false;
+            this.checkBoxDataFileLoaded.FlatAppearance.BorderSize = 0;
+            this.checkBoxDataFileLoaded.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBoxDataFileLoaded.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
+            this.checkBoxDataFileLoaded.Location = new System.Drawing.Point(225, 0);
+            this.checkBoxDataFileLoaded.Name = "checkBoxDataFileLoaded";
+            this.checkBoxDataFileLoaded.Padding = new System.Windows.Forms.Padding(10);
+            this.checkBoxDataFileLoaded.Size = new System.Drawing.Size(53, 77);
+            this.checkBoxDataFileLoaded.TabIndex = 2;
+            this.checkBoxDataFileLoaded.UseVisualStyleBackColor = true;
+            // 
+            // buttonRecallPreset
+            // 
+            this.buttonRecallPreset.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonRecallPreset.FlatAppearance.BorderSize = 0;
+            this.buttonRecallPreset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRecallPreset.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+            this.buttonRecallPreset.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
+            this.buttonRecallPreset.Location = new System.Drawing.Point(0, 373);
+            this.buttonRecallPreset.Name = "buttonRecallPreset";
+            this.buttonRecallPreset.Size = new System.Drawing.Size(766, 57);
+            this.buttonRecallPreset.TabIndex = 2;
+            this.buttonRecallPreset.Text = "Recall Existing Set";
+            this.buttonRecallPreset.UseVisualStyleBackColor = true;
+            // 
+            // panelFilesGlobal
+            // 
+            this.panelFilesGlobal.Controls.Add(this.buttonGatherData);
+            this.panelFilesGlobal.Controls.Add(this.panelFiles);
+            this.panelFilesGlobal.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelFilesGlobal.Location = new System.Drawing.Point(0, 0);
+            this.panelFilesGlobal.Name = "panelFilesGlobal";
+            this.panelFilesGlobal.Size = new System.Drawing.Size(766, 167);
+            this.panelFilesGlobal.TabIndex = 4;
+            // 
+            // panelFiles
+            // 
+            this.panelFiles.Controls.Add(this.panelDataFile);
+            this.panelFiles.Controls.Add(this.panelInfofile);
+            this.panelFiles.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelFiles.Location = new System.Drawing.Point(0, 0);
+            this.panelFiles.Name = "panelFiles";
+            this.panelFiles.Size = new System.Drawing.Size(278, 167);
+            this.panelFiles.TabIndex = 0;
+            // 
+            // panelInfofile
+            // 
+            this.panelInfofile.Controls.Add(this.checkBoxInfoFileLoaded);
+            this.panelInfofile.Controls.Add(this.panelInfoFileButtons);
+            this.panelInfofile.Controls.Add(this.labelVariablesInfoFile);
+            this.panelInfofile.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelInfofile.Location = new System.Drawing.Point(0, 0);
+            this.panelInfofile.Name = "panelInfofile";
+            this.panelInfofile.Size = new System.Drawing.Size(278, 90);
+            this.panelInfofile.TabIndex = 3;
+            // 
+            // panelDataFile
+            // 
+            this.panelDataFile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(63)))), ((int)(((byte)(60)))));
+            this.panelDataFile.Controls.Add(this.checkBoxDataFileLoaded);
+            this.panelDataFile.Controls.Add(this.panel8);
+            this.panelDataFile.Controls.Add(this.labelDataFile);
+            this.panelDataFile.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDataFile.Location = new System.Drawing.Point(0, 90);
+            this.panelDataFile.Name = "panelDataFile";
+            this.panelDataFile.Size = new System.Drawing.Size(278, 77);
+            this.panelDataFile.TabIndex = 4;
+            // 
+            // panelInfoFileButtons
+            // 
+            this.panelInfoFileButtons.Controls.Add(this.buttonEditInfoFile);
+            this.panelInfoFileButtons.Controls.Add(this.buttonBrowseInfofile);
+            this.panelInfoFileButtons.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelInfoFileButtons.Location = new System.Drawing.Point(118, 0);
+            this.panelInfoFileButtons.Name = "panelInfoFileButtons";
+            this.panelInfoFileButtons.Size = new System.Drawing.Size(107, 90);
+            this.panelInfoFileButtons.TabIndex = 1;
+            // 
+            // buttonEditInfoFile
+            // 
+            this.buttonEditInfoFile.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonEditInfoFile.FlatAppearance.BorderSize = 0;
+            this.buttonEditInfoFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonEditInfoFile.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+            this.buttonEditInfoFile.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
+            this.buttonEditInfoFile.Location = new System.Drawing.Point(0, 47);
+            this.buttonEditInfoFile.Name = "buttonEditInfoFile";
+            this.buttonEditInfoFile.Size = new System.Drawing.Size(107, 43);
+            this.buttonEditInfoFile.TabIndex = 2;
+            this.buttonEditInfoFile.Text = "Edit";
+            this.buttonEditInfoFile.UseVisualStyleBackColor = true;
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.buttonBrowseDatafile);
+            this.panel8.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel8.Location = new System.Drawing.Point(118, 0);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(107, 77);
+            this.panel8.TabIndex = 1;
+            // 
+            // buttonGatherData
+            // 
+            this.buttonGatherData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonGatherData.FlatAppearance.BorderSize = 0;
+            this.buttonGatherData.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonGatherData.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+            this.buttonGatherData.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
+            this.buttonGatherData.Location = new System.Drawing.Point(278, 0);
+            this.buttonGatherData.Name = "buttonGatherData";
+            this.buttonGatherData.Size = new System.Drawing.Size(488, 167);
+            this.buttonGatherData.TabIndex = 2;
+            this.buttonGatherData.Text = "Gather Data";
+            this.buttonGatherData.UseVisualStyleBackColor = true;
             // 
             // Set
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(83)))), ((int)(((byte)(79)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(766, 430);
+            this.Controls.Add(this.panelFilesGlobal);
+            this.Controls.Add(this.buttonRecallPreset);
             this.Name = "Set";
             this.Text = "Set";
+            this.panelFilesGlobal.ResumeLayout(false);
+            this.panelFiles.ResumeLayout(false);
+            this.panelInfofile.ResumeLayout(false);
+            this.panelDataFile.ResumeLayout(false);
+            this.panelDataFile.PerformLayout();
+            this.panelInfoFileButtons.ResumeLayout(false);
+            this.panel8.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelVariablesInfoFile;
+        private System.Windows.Forms.Button buttonBrowseInfofile;
+        private System.Windows.Forms.CheckBox checkBoxInfoFileLoaded;
+        private System.Windows.Forms.Label labelDataFile;
+        private System.Windows.Forms.Button buttonBrowseDatafile;
+        private System.Windows.Forms.CheckBox checkBoxDataFileLoaded;
+        private System.Windows.Forms.Button buttonRecallPreset;
+        private System.Windows.Forms.Panel panelFilesGlobal;
+        private System.Windows.Forms.Panel panelFiles;
+        private System.Windows.Forms.Panel panelDataFile;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panelInfofile;
+        private System.Windows.Forms.Panel panelInfoFileButtons;
+        private System.Windows.Forms.Button buttonEditInfoFile;
+        private System.Windows.Forms.Button buttonGatherData;
     }
 }
