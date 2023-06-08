3b8b6550a31112f49a7573453ec54e953c39412e
Gautzilla <gauthier.berthomieu@gmail.com>
2023-06-08 11:55:03 +0200

    Add set loading logic

--- DataPlotter/Forms/SetLoading.Designer.cs
+++ DataPlotter/Forms/SetLoading.Designer.cs
@@ -28,7 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonLoadPreset = new System.Windows.Forms.Button();
+            this.listViewChartInfos = new System.Windows.Forms.ListView();
             this.SuspendLayout();
+            // 
+            // buttonLoadPreset
+            // 
+            this.buttonLoadPreset.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonLoadPreset.FlatAppearance.BorderSize = 0;
+            this.buttonLoadPreset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLoadPreset.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+            this.buttonLoadPreset.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
+            this.buttonLoadPreset.Location = new System.Drawing.Point(0, 393);
+            this.buttonLoadPreset.Name = "buttonLoadPreset";
+            this.buttonLoadPreset.Size = new System.Drawing.Size(800, 57);
+            this.buttonLoadPreset.TabIndex = 3;
+            this.buttonLoadPreset.Text = "Load";
+            this.buttonLoadPreset.UseVisualStyleBackColor = true;
+            this.buttonLoadPreset.Click += new System.EventHandler(this.buttonLoadPreset_Click);
+            // 
+            // listViewChartInfos
+            // 
+            this.listViewChartInfos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(83)))), ((int)(((byte)(79)))));
+            this.listViewChartInfos.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listViewChartInfos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewChartInfos.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+            this.listViewChartInfos.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(231)))));
+            this.listViewChartInfos.FullRowSelect = true;
+            this.listViewChartInfos.HideSelection = false;
+            this.listViewChartInfos.Location = new System.Drawing.Point(0, 0);
+            this.listViewChartInfos.MultiSelect = false;
+            this.listViewChartInfos.Name = "listViewChartInfos";
+            this.listViewChartInfos.Size = new System.Drawing.Size(800, 393);
+            this.listViewChartInfos.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listViewChartInfos.TabIndex = 4;
+            this.listViewChartInfos.UseCompatibleStateImageBehavior = false;
             // 
             // SetLoading
             // 
@@ -36,6 +70,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(83)))), ((int)(((byte)(79)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listViewChartInfos);
+            this.Controls.Add(this.buttonLoadPreset);
             this.Name = "SetLoading";
             this.Text = "Set Loading";
             this.ResumeLayout(false);
@@ -43,5 +79,7 @@
         }
 
         #endregion
+        private System.Windows.Forms.Button buttonLoadPreset;
+        private System.Windows.Forms.ListView listViewChartInfos;
     }
 }
--- DataPlotter/Forms/SetLoading.cs
+++ DataPlotter/Forms/SetLoading.cs
@@ -1,22 +1,101 @@
-﻿using System;
+﻿using DataPlotter.DataPlotterLibrary;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace DataPlotter.Forms
 {
     public partial class SetLoading : Form
     {
         private Home _home;
+
+        private List<ChartInfo> _sets;
+        private JsonSerializer _serializer;
+
+        private string _setsFilePath;
+
         public SetLoading(Home home)
         {
             InitializeComponent();
             _home = home;
+            _sets = new List<ChartInfo>();
+            _serializer = new JsonSerializer();
+            CreateSetsFile();
+            GatherSavedSets();
+            CreateList();
+        }
+
+        private void CreateSetsFile()
+        {
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            _setsFilePath = documentsPath + @"\DataPlotter\presets.json";
+
+            if (File.Exists(_setsFilePath)) return;
+
+            Directory.CreateDirectory(documentsPath + @"\DataPlotter");
+            using (FileStream fs = File.Create(_setsFilePath)) { }
+        }
+
+        private void GatherSavedSets()
+        {
+            using (var streamReader = new StreamReader(_setsFilePath))
+            using (var jsonReader = new JsonTextReader(streamReader))
+            {
+                _sets = _serializer.Deserialize<List<ChartInfo>>(jsonReader) ?? new List<ChartInfo>();
+            }
+        }
+
+        private void CreateList()
+        {
+            listViewChartInfos.View = View.Details;
+            listViewChartInfos.Columns.Add("Data file");
+            listViewChartInfos.Columns.Add("Name");
+            listViewChartInfos.Columns.Add("X var");
+            listViewChartInfos.Columns.Add("Y Var");
+            listViewChartInfos.Columns.Add("Y Var2");
+
+            foreach (var chartInfo in _sets)
+            {
+                listViewChartInfos.Items.Add(CreateListItem(chartInfo));
+            }
+
+            for (int i = 0; i < listViewChartInfos.Columns.Count; i++)
+            {
+                listViewChartInfos.AutoResizeColumn(i, ColumnHeaderAutoResizeStyle.ColumnContent);
+            }
+        }
+
+        private ListViewItem CreateListItem(ChartInfo chartInfo)
+        {
+            ListViewItem item = new ListViewItem();
+            item.Text = Regex.Match(chartInfo.ID, @"[^\\]*\.csv").Value;
+            item.SubItems.Add(chartInfo.Name);
+            item.SubItems.Add(chartInfo.XVar);
+            item.SubItems.Add(chartInfo.YVar);
+            item.SubItems.Add(chartInfo.YVar2);
+            item.Tag = chartInfo;
+
+            return item;
+        }
+
+        private void buttonLoadPreset_Click(object sender, EventArgs e)
+        {
+            if (listViewChartInfos.SelectedItems.Count == 0) return;
+
+            _home.ChartInfo = (ChartInfo)listViewChartInfos.SelectedItems[0].Tag;
+
+            MessageBox.Show("The selected data set was successfully loaded.");
         }
     }
 }
